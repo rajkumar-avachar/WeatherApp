0cf73a0c95f987ff7f83dcd6ff9241021b16c040
Rajkumar Avachar <rajavachar59@gmail.com>
2025-11-19 22:04:51 +0530

    change ui

--- src/WeatherApp/Details.jsx
+++ src/WeatherApp/Details.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import './Details.css'
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import React from "react";
+import "./style.css";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
 function Details({ info }) {
-    return (
-        <div className="container mt-5">
-            <div className="row border border-black w-75 m-auto rounded-4 ps-4 pe-4 pt-4 pb-5">
-                <h1 className='fw-bold'>{info.city}</h1>
-                <p className='fs-1 fw-bold mt-3 ms-2'> <WbSunnyIcon className='fs-1 text-warning fw-bold' /> {info.temp}&deg; C</p>
-                <h2 className='fw-bold text-success'>Weather: &nbsp;{info.weather}</h2>
-                <div className='d-flex gap-5 text-center moreDetails mt-5 flex-wrap'>
-                    <div>Feels Like <br /> <h4>{info.feelsLike}&deg;</h4></div>
-                    <div>Humidity <br /> <h4>{info.humidity}%</h4></div>
-                    <div>Wind <br /> <h4>{info.wind}km/h</h4></div>
-                    <div>Pressure <br /> <h4>{info.pressure} hPa</h4></div>
-                </div>
-            </div>
+  return (
+    <div className="mt-4 mx-auto " style={{ width: "50rem" }}>
+      <div className="row border  m-auto rounded-4 ps-4 pe-4 pt-4 pb-5 bg-light">
+        <div className="d-flex align-items-center justify-content-between">
+          <h1 className="fw-bold">{info.city}</h1>
+          <p className="fs-1 fw-bold mt-3 ms-2">
+            <WbSunnyIcon className="fs-1 text-warning fw-bold" /> {info.temp}
+            &deg; C
+          </p>
         </div>
-    );
+        <h2 className="fw-bold text-center mt-2">{info.weather}</h2>
+
+        {/* 2x2 grid for details */}
+        <div className="row text-center mt-5">
+          <div className="col-6 mb-4">
+            <div className="border rounded-4 p-3 bg-white">
+              Feels Like <br />
+              <h4 className="mt-2">{info.feelsLike}&deg;</h4>
+            </div>
+          </div>
+          <div className="col-6 mb-4">
+            <div className="border rounded-4 p-3 bg-white">
+              Humidity <br />
+              <h4 className="mt-2">{info.humidity}%</h4>
+            </div>
+          </div>
+          <div className="col-6 mb-4">
+            <div className="border rounded-4 p-3 bg-white">
+              Wind <br />
+              <h4 className="mt-2">{info.wind} km/h</h4>
+            </div>
+          </div>
+          <div className="col-6 mb-4">
+            <div className="border rounded-4 p-3 bg-white">
+              Pressure <br />
+              <h4 className="mt-2">{info.pressure} hPa</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Details;
--- src/WeatherApp/WeatherPage.jsx
+++ src/WeatherApp/WeatherPage.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import Search from './Search';
-import Details from './Details';
-import { useState } from 'react';
-
+import React from "react";
+import Search from "./Search";
+import Details from "./Details";
+import { useState } from "react";
 
 function WeatherPage() {
     const [weatherInfo, setWeatherInfo] = useState({
-        city: "Pune",
-        temp: 25,
-        feelsLike: 20,
-        humidity: 56,
-        pressure: 1023,
-        weather: "Sunny",
-        wind: 5,
+      city: "City",
+      temp: "",
+      feelsLike: "",
+      humidity: "",
+      pressure: "",
+      weather: "",
+      wind: "",
     });
 
-    let updateInfo = (result) => {
-        setWeatherInfo(result);
 
-    }
-    return (
-        <>
-            <Search updateInfo={updateInfo}/>
-            <Details info={weatherInfo}/>
-        </>
-    );
+  let updateInfo = (result) => {
+    setWeatherInfo(result);
+  };
+  return (
+    <div className="container">
+      <Search updateInfo={updateInfo} />
+      <Details info={weatherInfo} />
+    </div>
+  );
 }
 
 export default WeatherPage;
